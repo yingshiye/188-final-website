380f5f7495c875157708612704f0e34dfed7cd59
Yingshi Y <72583571+yingshiye@users.noreply.github.com>
2025-06-06 23:32:14 -0700

    ..

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import ProjectSummary from '@/components/ProjectSummary';
+import ProjectSummary from '../components/ProjectSummary';
 
 export default function Home() {
   return <ProjectSummary />;
